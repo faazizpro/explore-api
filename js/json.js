10a8c5841826d34ebed7e66a39cf12c4f6b2130e
faazizpro <fahadafs11@gmail.com>
2022-08-27 12:48:52 +0600

    JSON, JSON Structure, parse, stringify

--- js/json.js
+++ js/json.js
@@ -1,5 +1,21 @@
 const user = {
-    id: 1, name: 'Fahad Abdul Aziz', job: 'Web Developer', 
-};
+    id: 1, name: 'Fahad Abdul Aziz', job: 'Web Developer', };
+//JavaScript Object Notation(JSON)
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
 
-console.log(user);
+const shop = {
+    onwer: 'Fahad Abdul Aziz',
+    location: 'Banasree',
+    country: 'Bangladesh',
+    salary: 135000
+}
+console.log(shop);
+// Convert Shop to JSON String
+const shopString = JSON.stringify(shop);
+console.log(shopString);
+
+// COnvert JSON String to Object
+const shopObject = JSON.parse(shopString);
+console.log(shopObject);
